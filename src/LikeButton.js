ca1054d06703bb12b0d0f0fc4c7f60f1f01069cd
Alex Heywood <alexheywood@valeofglamorgan.gov.uk>
2023-10-20 10:55:08 +0100

    added a love heart like icon

--- src/LikeButton.js
+++ src/LikeButton.js
@@ -5,28 +5,36 @@ function LikeButton()
 {
     const [isLiked, setLike] = useState(false);
     let [likes, setLikes] = useState(3234);
+    let [buttonText, setButtonText] = useState('Like');
+    let [buttonURL, setButtonURL] = useState('noLike.png');
 
     const handleBid = () => {
 
         if(isLiked) {
             setLikes((likes - 1));
             setLike(false);
+            setButtonText('Like');
+            setButtonURL('noLike.png');
             
         }
         else {
         setLike(true);
         setLikes((likes + 1));
+        setButtonText('Unlike');
+        setButtonURL('like.png');
         }
         console.log(isLiked);
         console.log("Likes: " + likes);
+        
     }
 
-
+ 
     return (
-        <div className="bidSection d-flex">
-            <p>Count: {likes}</p>
-            <div className="bidButton">
-            <input type="submit" value="Like" onClick={handleBid} />
+        <div className="bidSection justify-space-even">
+                        <img src={buttonURL} width="30" height="30"/>
+            <p className="likeCount">Count: {likes}</p>
+            <div className="likeButton">
+            <input type="submit" value={buttonText} onClick={handleBid} />
             </div>
         </div>
     )
